6b3638bd01dd0b248cc4b771d06e9e7bcff5cd40
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2023-12-13 22:42:37 +0000

    done

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -10,23 +10,21 @@ function AllBeersPage() {
     const getBeers = async () => {
         try {
             const result = await axios.get(BEERS_API);
-            console.log(result.data);
+            // console.log(result.data);
             setBeers(result.data);
         } catch (error) {
             console.log(error);
         }
     }
 
-    // const editContributor = async (value) => {
-    //     try {
-    //         if (value.includes('<')) {
-    //             const result = await value.split('<')[0].trim();
-    //             return result;
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    const handleSearch = async (query) => {
+        try {
+            const result = await axios.get(`${BEERS_API}/search?q=${query}`)
+            setBeers(result.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     useEffect(() => {
         getBeers();
@@ -35,6 +33,13 @@ function AllBeersPage() {
 
     return (
         <div>
+            <div className="search-bar">
+                <label htmlFor="filter" className="search-label">Search</label>
+                <input type="text" id="filter" className="search-input" onChange={(e) => {
+                    handleSearch(e.target.value);
+                }} />
+            </div>
+
             {beers.map((beer) => {
                 return (
                     <div key={beer._id}>
@@ -46,7 +51,7 @@ function AllBeersPage() {
                                 <div className="card-text">
                                     <h2 className="beer-name">{beer.name}</h2>
                                     <h3 className="tagline">{beer.tagline}</h3>
-                                    <p><strong>Created by:</strong> {beer.contributed_by}</p>
+                                    <p><strong>Created by:</strong> {beer.contributed_by && beer.contributed_by.split('<')[0].trim()}</p>
                                 </div>
                             </div>
                         </Link>
